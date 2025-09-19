318f56d999fb803cca820b8594324653a8362d75
Bui Thanh Tung <buithanhtungaabb@gmail.com>
2025-09-19 13:30:18 +0700

    Buoc 1 + 2

---
+++ src/main/java/vn/iotstar/Bt07BttApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt07BttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt07BttApplication.class, args);
+	}
+
+}
