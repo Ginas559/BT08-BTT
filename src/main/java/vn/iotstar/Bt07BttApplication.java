05b9811f9ca2f7dcebc85f9855b69d15335995ec
Bui Thanh Tung <buithanhtungaabb@gmail.com>
2025-09-22 19:32:31 +0700

    CẤU HÌNH SWAGGER2 VÀ SWAGGER 3 TRÊN SPRING BOOT

--- src/main/java/vn/iotstar/Bt07BttApplication.java
+++ src/main/java/vn/iotstar/Bt07BttApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+
 import vn.iotstar.config.StorageProperties;
 import vn.iotstar.service.IStorageService;
 
@@ -23,5 +30,25 @@ public class Bt07BttApplication {
 		return (args -> {
 			storageService.init();
 		});
+	}
+	
+	@Bean
+	public GroupedOpenApi publicApi() {
+	    return GroupedOpenApi.builder()
+	            .group("public")
+	            .pathsToMatch("/**")
+	            .build();
+	}
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+	    return new OpenAPI()
+	            .info(new Info().title("BT07-BTT API")
+	            .description("Bùi Thanh Tùng - 23110357")
+	            .version("v1.0")
+	            .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+	            .externalDocs(new ExternalDocumentation()
+	            .description("SpringShop Wiki Documentation")
+	            .url("https://springshop.wiki.github.org/docs"));
 	}
 }
